18a0004ff1ab6227315110866f4ff30e4ae9d4f7
David <david.gawin@gmail.com>
2025-04-18 11:50:43 +0200

    Renamed Rebirth to Prestige

---
+++ src/scenes/PrestigeMenu.js
@@ -0,0 +1,31 @@
+export class PrestigeMenu {
+    constructor(gameScene){
+        this.gameScene = gameScene
+        this.config = gameScene.config
+        this.rebirths = 0;
+
+
+
+
+        this.toggleMenu(true)
+    }
+
+
+    updateRebirthProgress(){
+        return;
+    }
+
+    toggleMenu(isActive){
+
+        return;
+    }
+
+    isRebirthRequirementsMet(){
+        return false;
+    }
+    
+    completeRebirth(){
+        return;
+    }
+
+}
